35850e08b6c100dbd2761711d4c01a63527bd708
MoyeRon <810247798@qq.com>
2022-01-07 00:08:09 +0800

    2022年1月7日00:08:06

---
+++ src/test/java/dailytraining/Solution1576Test.java
@@ -0,0 +1,11 @@
+package dailytraining;
+
+import org.junit.Test;
+
+public class Solution1576Test {
+    @Test
+    public void testModifyString() {
+        Solution1576 solution1576 = new Solution1576();
+        System.out.println(solution1576.modifyString("z?a"));
+    }
+}
