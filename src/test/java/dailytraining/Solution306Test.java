bfcf58977b7794da6ed20b01b6e102ae21b2d838
MoyeRon <810247798@qq.com>
2022-01-11 00:00:05 +0800

    2022年1月11日00:00:02

---
+++ src/test/java/dailytraining/Solution306Test.java
@@ -0,0 +1,14 @@
+package dailytraining;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class Solution306Test {
+    @Test
+    public void testIsAdditiveNumber() {
+        Solution306 solution306 = new Solution306();
+        // assertEquals(true, solution306.isAdditiveNumber("112358"));
+        assertEquals(true, solution306.isAdditiveNumber("199100199"));
+    }
+}
