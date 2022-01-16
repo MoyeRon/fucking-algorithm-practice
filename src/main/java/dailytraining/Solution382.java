0e37495b9bbd0abc8fbb0517e9f92f75aef71d64
MoyeRon <810247798@qq.com>
2022-01-16 11:11:54 +0800

    2022年1月16日11:11:49

---
+++ src/main/java/dailytraining/Solution382.java
@@ -0,0 +1,37 @@
+package dailytraining;
+
+import common.ListNode;
+
+import java.util.Random;
+
+public class Solution382 {
+
+    ListNode head;
+    ListNode tail;
+    ListNode current;
+    Random random = new Random();
+    int length = 1;
+
+    public Solution382(ListNode head) {
+        this.head = head;
+        current = this.head;
+        while (current.next != null) {
+            current = current.next;
+            length++;
+        }
+        tail = current;
+        current = head;
+    }
+
+    public int getRandom() {
+        int step = Math.abs(random.nextInt() % length);
+        for (int i = 0; i < step; i++) {
+            if (current == tail) {
+                current = head;
+            } else {
+                current = current.next;
+            }
+        }
+        return current.val;
+    }
+}
