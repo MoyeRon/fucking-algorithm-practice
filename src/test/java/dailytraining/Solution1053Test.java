f6748ce7040d106878e1dc053bb9950b35330da6
MoyeRon <810247798@qq.com>
2023-04-03 23:31:42 +0800

    2023年4月3日23:31:37

---
+++ src/test/java/dailytraining/Solution1053Test.java
@@ -0,0 +1,15 @@
+package dailytraining;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Solution1053Test {
+
+    @Test
+    public void prevPermOpt1Test() {
+        Solution1053 solution = new Solution1053();
+        int[] ints = solution.prevPermOpt1(new int[]{1, 9, 4, 6, 7});
+        int m=0;
+    }
+}
